a56037887be37c22b631c4474ee9981a5e069ffd
Muskan Jain <muskanj8642@gmail.com>
2026-03-09 16:17:13 +0530

    CSS units and variables

--- Python/title_fun.py
+++ Python/title_fun.py
@@ -1,7 +1,16 @@
 # Title(): it is a inbuilt fun which is used to capitalize first letter after every space 
 
-a = 'python is very easy'
+a = 'python is #vEry easy'
 b = ["python",'is','very','easy']
 print(a.title())
 # print(b.title())
 # print(" ".join(b.title()))
+
+a = 'python is #vEry easy'
+out = ''
+for i in range(len(a)):
+    if i == 0 or a[i-1] == ' ':
+        out += a[i].upper()
+    else:
+        out += a[i].lower()
+print(out)
