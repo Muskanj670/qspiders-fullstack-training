c6460d3d0d3dd7c5b11901033307c4d32b6fddea
Muskan Jain <muskanj8642@gmail.com>
2026-04-02 18:38:15 +0530

    Filter records using GET method

--- Django/FeedbackProject/myapp/urls.py
+++ Django/FeedbackProject/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('form/', views.form),
-    path('feedback/',views.feedback)
-
+    path('',views.home, name = 'home'),
+    path('form/', views.form, name= 'form'),
+    path('feedback/',views.feedback, name = 'feedback'),
 ]
--- Django/FeedbackProject/myapp/views.py
+++ Django/FeedbackProject/myapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import FeedbackModel
 # Create your views here.
 
+def home(request):
+    return render(request,'myapp/index.html')
+
 def form(request):
     submitted = False
     if request.method == 'POST':
@@ -12,7 +15,11 @@ def form(request):
     return render(request,'myapp/form.html',{'submitted':submitted})
 
 def feedback(request):
-    feedback = FeedbackModel.objects.all()
+    search_query = request.GET.get('search')
+    if search_query:
+        feedback = FeedbackModel.objects.filter(name__icontains = search_query)
+    else:
+        feedback = FeedbackModel.objects.all()
     context = {"feedbacks": feedback}
     return render(request,'myapp/feedback.html',context)
 
